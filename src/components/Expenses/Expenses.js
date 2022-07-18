46f3da03368d685e67dba2232c6fc4b7f1fcff5a
Zeussssssss <adi.kr02@yahoo.in>
2022-07-18 18:14:36 +0530

    Filter Updated

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,36 +7,36 @@ import ExpenseList from "./ExpenseList";
 import ExpenseChart from "./ExpenseChart";
 
 const Expenses = (props) => {
-  const [selectedYear, setSelectedYear] = useState(2022);
+  const [selectedYear, setSelectedYear] = useState("0000");
 
   const onYearSelectHandler = (yearOption) => {
     setSelectedYear(yearOption);
   };
 
-  const expenses = props.expenseArray
-    .filter((exp) => {
+  let expenses = props.expenseArray;
+  if (selectedYear === "0000") {
+    expenses = props.expenseArray;
+  } else {
+    expenses = props.expenseArray.filter((exp) => {
       const date = new Date(exp.date);
       return date.getFullYear() === selectedYear;
-    })
-    .map((exp) => (
-      <ExpenseItem
-        key={exp.id}
-        item={exp.title}
-        date={exp.date}
-        expense={exp.expense}
-      />
-    ));
+    });
+  }
+
+  const expenseTags = expenses.map((exp) => (
+    <ExpenseItem
+      key={exp.id}
+      item={exp.title}
+      date={exp.date}
+      expense={exp.expense}
+    />
+  ));
 
   return (
     <Card className="expenses">
       <ExpensesFilter onYearSelect={onYearSelectHandler}></ExpensesFilter>
-      <ExpenseChart
-        expenseArray={props.expenseArray.filter((exp) => {
-          const date = new Date(exp.date);
-          return date.getFullYear() === selectedYear;
-        })}
-      />
-      <ExpenseList expenseList={expenses} />
+      <ExpenseChart expenseArray={expenses} />
+      <ExpenseList expenseList={expenseTags} />
     </Card>
   );
 };
