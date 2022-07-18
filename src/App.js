ff0ba3c4126c76f298246b72002200caf8ec0729
Zeussssssss <adi.kr02@yahoo.in>
2022-07-18 17:47:33 +0530

    expense array reset

--- src/App.js
+++ src/App.js
@@ -5,16 +5,13 @@ import React, { useState } from "react";
 import Cookies from "universal-cookie";
 
 const App = () => {
-  const [expenses, setExpenses] = useState([
-    { id: 1, title: "Chocolate", date: new Date(2022, 5, 3), expense: 13 },
-    { id: 2, title: "Newspaper", date: new Date(2022, 3, 5), expense: 5 },
-    { id: 3, title: "Smartphone", date: new Date(2022, 9, 3), expense: 500 },
-  ]);
+  const [expenses, setExpenses] = useState([]);
 
   const cookie = new Cookies();
-  if (typeof cookie.get("expenses") == undefined)
+  console.log(typeof cookie.get("expenses"));
+  if (typeof cookie.get("expenses") == "undefined") {
     cookie.set("expenses", expenses, { path: "/" });
-
+  }
   const onUpdateHandler = (arr) => {
     cookie.set("expenses", arr, { path: "/" });
   };
